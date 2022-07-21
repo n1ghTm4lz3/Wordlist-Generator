e3d07a959b8f29f8e32a27513a1e31628dfaef26
No1lz <No1lz@protonmail.com>
2022-07-21 09:53:24 +0800

    [!] update Cellphone Generation Script

--- scripts/generate_cellphone.py
+++ scripts/generate_cellphone.py
@@ -3,15 +3,26 @@ prefixes = ['0910', '0911', '0912', '0913', '0914', '0915', '0916', '0917', '091
 			'0930', '0931', '0932', '0933', '0934', '0935', '0936', '0937', '0938', '0939', 
 			'0952', '0953', '0954', '0955', '0956', '0958', 
 			'0960', '0961', '0963', 
-			'0970', '0971', '0972', '0975', '0978', 
+			'0970', '0971', '0972', '0975', '0976', '0978', 
 			'0987', '0988'
-			] 
-for prefix in prefixes:
+			]
+baseLocation = "../wordlists/Taiwan-cellphone/"
+
+def Generate(prefix):
 	print("[+] Generating Prefix " + prefix + " Wordlist ...", end = '')
-	filename = "../wordlists/Taiwan-cellphone/" + prefix
+	filename = baseLocation + prefix
 	with open(filename, 'w') as f:
 		for i in range(1000000):
 			number = prefix + "%06d" % i + "\n"
 			f.write(number)
-	print("Done")
-print("[!] Complete")
+		print("Done")
+
+
+if __name__ == '__main__':
+	prefix = input("Input the prefix: ")
+	if prefix == "":
+		for prefix in prefixes:
+			Generate(prefix)
+	else:
+		Generate(prefix)
+	print("[!] Complete")
